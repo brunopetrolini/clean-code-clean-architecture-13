e8d2d0ed02746e44552be5f355fbd687f418b58a
Bruno Petrolini <brunopetrolini@hotmail.com>
2023-11-03 10:41:36 -0300

    style: adds eslint

--- src/Circle.ts
+++ src/Circle.ts
@@ -1,7 +1,7 @@
 export class Circle {
-  constructor(readonly radius: number) { }
+  constructor(readonly radius: number) {}
 
   public getArea(): number {
-    return 2 * Math.PI * this.radius;
+    return 2 * Math.PI * this.radius
   }
 }
--- test/Circle.test.ts
+++ test/Circle.test.ts
@@ -1,6 +1,6 @@
-import { Circle } from "../src/Circle";
+import { Circle } from '../src/Circle'
 
-test("Should calculate the area of circle", function () {
-  const circle = new Circle(2);
-  expect(circle.getArea()).toBe(12.566370614359172);
-});
+test('Should calculate the area of circle', function () {
+  const circle = new Circle(2)
+  expect(circle.getArea()).toBe(12.566370614359172)
+})
